c0280d65b77d6ec84713678aa24d13f02dd3abf4
Anton <revizor2626@yandex.ru>
2018-07-05 15:03:36 +0300

    Выполнено редактирование кода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/TestClass.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/TestClass.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class TestClass {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
         System.out.println("Hello world");
 
