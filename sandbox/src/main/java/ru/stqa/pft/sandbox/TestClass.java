395af4355672618cbb409641db06cce1a022bb18
Anton <revizor2626@yandex.ru>
2018-07-12 11:20:45 +0300

    Добавил новые методы и классы для вычисления площади

--- sandbox/src/main/java/ru/stqa/pft/sandbox/TestClass.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/TestClass.java
@@ -10,13 +10,11 @@ public class TestClass {
         hello("user");
         hello("Alexei");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
     }
 
@@ -24,14 +22,9 @@ public class TestClass {
         System.out.println("Hello," + somebody + "!");
     }
 
-    public static double area (double len) {
-        return len * len;
 
-    }
 
-    public static double area(double a, double b){
-        return a * b;
-    }
+
 
 
 }
