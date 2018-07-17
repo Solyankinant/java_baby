5cc6d759782064328ced4cadd355a6f326ca7a47
Anton <revizor2626@yandex.ru>
2018-07-17 10:44:17 +0300

    Удалил не нужные библиотеки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -5,15 +5,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
+
 
 public class GroupCreationTest {
     ChromeDriver wd;
